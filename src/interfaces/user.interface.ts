326d48d15e2dd0e796249a97ba3a1f887fd8dc78
Agustin <agustin-avero@hotmail.com>
2021-05-10 20:41:16 -0300

    Primera versión con la gestion de usuarios terminada: register, login, me, users

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+    id?: string;
+    user: string;
+    name: string;
+    lastname: string;
+    email: string;
+    password: string;
+    registerDate: string;
+    birthday: string;
+    role: string;
+}
